e524fe43ca94ed7b2cc748ac73ceeb32609d0819
TopoH19 <37462166+TopoH19@users.noreply.github.com>
2024-07-03 11:59:54 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient} from '@angular/common/http';
 import { subscribe } from 'diagnostics_channel';
@@ -10,6 +10,7 @@ import { subscribe } from 'diagnostics_channel';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+ 
 export class AppComponent {
   data: any [] = [];
   title = 'applicationTest';
@@ -18,9 +19,10 @@ export class AppComponent {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.http.get('')// colocar la api
+    this.http.get('https://pokeapi.co/api/v2/pokemon')// colocar la api
     .subscribe((data: any) => {
       this.data = data;
     })
   }
+  
 }
